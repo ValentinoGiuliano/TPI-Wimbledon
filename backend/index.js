499ee61e2ad8d5ee9fe0ca4f59e31036a27cc191
Valentino Giuliano <94819@sistemas.frc.utn.edu.ar>
2024-07-02 00:06:17 -0300

    Etapa 1 completa

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+// index.js
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend inicial Wimbledon!");
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`sitio escuchando en el puerto ${port}`);
+});
